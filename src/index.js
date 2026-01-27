3cdcefb45bb7e9cacf86bd93861f0945b580b996
sythoniq <188770367+sythoniq@users.noreply.github.com>
2026-01-27 19:18:46 +0300

    completely implemented the project

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const path = require("node:path");
+const express = require("express");
+const app = express();
+
+const index = require("./routes/indexRoute");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({extended:true}));
+app.use("/", index);
+app.use("/new", index)
+app.use("/message/:id", index);
+
+app.listen(3000, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log("Server now running on port 3000");
+})
---
+++ src/routes/indexRoute.js
@@ -0,0 +1,39 @@
+const { Router } = require("express");
+const index = Router();
+
+const messages = [
+  {
+    text: "Hi there",
+    user: "Amando",
+    added: new Date(),
+    id: 1,
+  },
+  {
+    text: "Hello world!",
+    user: "Charles",
+    added: new Date(),
+    id: 2,
+  }
+]
+
+index.get("/", (req, res) => {
+  res.render("index", { title: "Mini msg board", messages: messages})
+})
+
+index.get("/new", (req, res) => {
+  res.render("form", {title: "New message"});
+})
+
+index.post("/new", (req, res) => {
+  const msg = req.body;
+  messages.push({ text: msg.textContent, user: msg.userName, added: new Date(), id: messages.length + 1 });
+  res.redirect("/");
+})
+
+index.get("/message/:id", (req, res) => {
+  const message = messages.find((message) => message.id == req.params.id);
+  console.log(req.params.id, messages);
+  res.render("detail", {title: "Message Detail", message: message});
+})
+
+module.exports = index;
